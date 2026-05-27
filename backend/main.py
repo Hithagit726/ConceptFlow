46fe659a00f628d6c1f1fd6e38528c7230c5c8c1
Hitha <hittthaaa27@gmail.com>
2026-05-27 15:36:30 +0530

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -7,9 +7,10 @@ load_dotenv()
 
 app = FastAPI(title="Rabbit Hole Mapper API")
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
